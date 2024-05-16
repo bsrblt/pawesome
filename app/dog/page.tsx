bebc9b2fea4fe93cf79bf3035e0b6676c2c3883f
Ferhat Başar Bulut <ferhatbasarbulut@gmail.com>
2024-05-16 23:33:03 +0300

    initial commit

--- app/dog/page.tsx
+++ app/dog/page.tsx
@@ -1,3 +1,5 @@
+import Button from "components/ui/Button";
+import Card from "components/ui/Card";
 import React from "react";
 
 const Dog = () => {
@@ -11,7 +13,35 @@ const Dog = () => {
           backgroundPosition: "center",
         }}
       ></div>
-      <div className="container mx-auto px-4 grid md:grid-cols-2 gap-8 items-center relative z-10"></div>
+
+      <div className="container mx-auto px-4 items-center relative z-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          <Card
+            imageSrc="/dogfood.jpg"
+            title="Premium Dry Dog Food"
+            description="High-quality protein and essential nutrients for your pup."
+            price={49.99}
+          />
+          <Card
+            imageSrc="/dogfoodwet.jpg"
+            title="Wet Dog Food"
+            description="Delicious and nutritious wet food for your canine friend."
+            price={39.99}
+          />
+          <Card
+            imageSrc="/dogfood3.jpg"
+            title="Puppy Food"
+            description="Specially formulated and highly nutritious for growing puppies."
+            price={29.99}
+          />
+          <Card
+            imageSrc="/dog1.jpg"
+            title="Senior Dog Food"
+            description="Tailored nutrition carefully crafted for your older canine companion."
+            price={29.99}
+          />
+        </div>
+      </div>
     </section>
   );
 };
